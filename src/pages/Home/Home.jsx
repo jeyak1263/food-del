277283ef69e7b7a8b372c9dabb108a8b178e7a98
Jey <jeyak1263@gmail.com>
2025-12-09 12:12:19 +0530

    85%

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import Section from "../../components/Section/Section";
-import Exploremenu from "../../components/Exploremenu/exploremenu";
+import Exploremenu from "../../components/Exploremenu/Exploremenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import Footer from "../../components/Footer/Footer";
 import AppDownload from "../../components/AppDownload/AppDownload";
